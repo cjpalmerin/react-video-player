3f2dc78dce073b2dfdb996d0e72641a2eecb6426
CJPALME <cjpalmerin@gmail.com>
2020-07-20 15:44:56 -0500

    created server for future deploy

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+//STATIC ASSETS
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+//SERVER LISTENING
+app.listen(PORT, function() {
+    console.log(`Server is listening on http://localhost/$(PORT)`)
+})
